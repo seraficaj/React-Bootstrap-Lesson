54328839b78975f2aebe28b3fa492f8c24887c08
Jason Serafica <seraficaj@gmail.com>
2022-03-21 10:20:50 -0700

    add background color and styling to navbar

--- src/Components/MainNavbar.jsx
+++ src/Components/MainNavbar.jsx
@@ -1,22 +1,32 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+import "./MainNavbar.css";
+
 const MainNavbar = () => (
-  <Navbar bg="light" expand="md">
-    <Container>
-      <Navbar.Brand><Link to="/">React Pokedex</Link></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-            <Link className="nav-link" to="/">PokeDex</Link>
-            <Link className="nav-link" to="/myteam">MyTeam</Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+    <Navbar bg="primary" variant="dark" expand="md">
+        <Container>
+            <Navbar.Brand>
+                <Link style={{ color: "white", textDecoration: "none" }} to="/">
+                    React Pokedex
+                </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Link className="nav-link" to="/">
+                        PokeDex
+                    </Link>
+                    <Link className="nav-link" to="/myteam">
+                        MyTeam
+                    </Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
 );
 
 export default MainNavbar;
